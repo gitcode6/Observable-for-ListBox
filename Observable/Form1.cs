42d9e67a9dce515754a6401f57bb8b6bea0d5121
Tobias <Tobias@DESKTOP-N8MRQ46>
2022-11-13 15:14:09 +0100

    Projektdateien hinzufügen.

---
+++ Observable/Form1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Observable
+{
+    public partial class Form1 : Form
+    {
+        BindingList<Person> people = new BindingList<Person>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Sample.AddPeople(people);
+            listBox1.DataSource = people;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string firstName = Interaction.InputBox("Gib deinen Vornamen ein");
+            string lastName = Interaction.InputBox("Gib deinen Nachnamen ein");
+            int age = Convert.ToInt32(Interaction.InputBox("Gib dein Alter ein"));
+
+            people.Add(new Person() { FirstName = firstName, LastName = lastName, Age = age });
+        }
+
+    }
+}
---
+++ Observable/Sample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observable
+{
+    public static class Sample
+    {
+        public static void AddPeople(BindingList<Person> people)
+        {
+            people.Add(new Person() { FirstName = "Tobias", LastName = "Lehner", Age = 17 });
+            people.Add(new Person() { FirstName = "Alexander", LastName = "Schmidt", Age = 16 });
+            people.Add(new Person() { FirstName = "Pia", LastName = "Kotzbek", Age = 16 });
+        }
+    }
+}
